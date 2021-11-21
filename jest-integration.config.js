d381a00540ed60ec24702f779c59e0b30f324ec6
Dyarlen Iber <dyarlen1@gmail.com>
2021-11-21 01:33:45 +0100

    chore: add jest configurations and scripts

---
+++ jest-integration.config.js
@@ -0,0 +1,4 @@
+const config = require('./jest.config');
+
+config.testMatch = ['**/*.test.ts', '**/*.test.js'];
+module.exports = config;
---
+++ jest-unit.config.js
@@ -0,0 +1,4 @@
+const config = require('./jest.config');
+
+config.testMatch = ['**/*.spec.ts', '**/*.spec.js'];
+module.exports = config;
