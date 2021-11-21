f6cbc9c991e1b56518c6e6c347df0a79263c228f
Dyarlen Iber <dyarlen1@gmail.com>
2021-11-21 03:20:25 +0100

    feat: add email validator adapter

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,8 @@
+import validator from 'validator';
+import { EmailValidator } from '../presentation/protocols';
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid(email: string): boolean {
+    return validator.isEmail(email);
+  }
+}
