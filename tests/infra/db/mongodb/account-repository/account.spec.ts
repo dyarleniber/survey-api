d9c7320b62d2a055e665616c0b262e138795f307
Dyarlen Iber <dyarlen1@gmail.com>
2021-11-29 00:03:38 +0100

    test: ensure AccountMongoRepository returns account on success

---
+++ tests/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '../../../../../src/infra/db/mongodb/helpers/mongo-helper';
+import { AccountMongoRepository } from '../../../../../src/infra/db/mongodb/account-repository/account';
+
+const makeSut = (): AccountMongoRepository => new AccountMongoRepository();
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async (): Promise<void> => {
+    await MongoHelper.connect(<string>process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should return an account on success', async () => {
+    const sut = makeSut();
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email@mail.com');
+    expect(account.password).toBe('any_password');
+  });
+});
