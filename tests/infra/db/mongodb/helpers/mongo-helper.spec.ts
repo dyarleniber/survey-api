12391cef10db0111e5084e6d2a6d1633ef45f653
Dyarlen Iber <dyarlen1@gmail.com>
2021-11-30 22:04:24 +0100

    test: ensure mongodb reconnect if client is down

---
+++ tests/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,22 @@
+import { MongoHelper } from '../../../../../src/infra/db/mongodb/helpers/mongo-helper';
+
+const makeSut = () => MongoHelper;
+
+describe('Mongo Helper', () => {
+  beforeAll(async (): Promise<void> => {
+    await MongoHelper.connect(<string>process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should reconnect if mongodb is down', async () => {
+    const sut = makeSut();
+    let accountCollection = await sut.getCollection('accounts');
+    expect(accountCollection).toBeTruthy();
+    await sut.disconnect();
+    accountCollection = await sut.getCollection('accounts');
+    expect(accountCollection).toBeTruthy();
+  });
+});
