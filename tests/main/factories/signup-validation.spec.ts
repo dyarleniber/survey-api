830bda05d280b3078d223ad65a8e522a3a93f2f1
Dyarlen Iber <dyarlen1@gmail.com>
2021-12-05 16:00:38 +0100

    test: ensure makeSignUpValidation calls ValidationComposite with all validations

--- tests/main/factories/signup-validation.spec.ts
+++ tests/main/factories/signup-validation.spec.ts
@@ -1,19 +1,19 @@
 import { makeSignUpValidation } from '../../../src/main/factories/signup-validation';
-import { Validation } from '../../../src/presentation/helpers/validators/validation';
 import { ValidationComposite } from '../../../src/presentation/helpers/validators/validation-composite';
 import { RequiredFieldValidation } from '../../../src/presentation/helpers/validators/required-field-validation';
+import { CompareFieldsValidation } from '../../../src/presentation/helpers/validators/compare-fields-validation';
 
 jest.mock('../../../src/presentation/helpers/validators/validation-composite');
 
 describe('SignUp Validation', () => {
   test('Should call ValidationComposite with all validations', () => {
     makeSignUpValidation();
-    const validations: Validation[] = [
-      'name',
-      'email',
-      'password',
-      'passwordConfirmation',
-    ].map((field) => new RequiredFieldValidation(field));
-    expect(ValidationComposite).toHaveBeenCalledWith(validations);
+    expect(ValidationComposite).toHaveBeenCalledWith([
+      new RequiredFieldValidation('name'),
+      new RequiredFieldValidation('email'),
+      new RequiredFieldValidation('password'),
+      new RequiredFieldValidation('passwordConfirmation'),
+      new CompareFieldsValidation('password', 'passwordConfirmation'),
+    ]);
   });
 });
