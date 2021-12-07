4db7a8a2d040a4ab938b52e9dd33dfb8581c7420
Dyarlen Iber <dyarlen1@gmail.com>
2021-12-07 14:56:25 +0100

    test: ensure RequiredFieldValidation not return if validation succeeds

--- tests/presentation/helpers/validators/required-field-validation.spec.ts
+++ tests/presentation/helpers/validators/required-field-validation.spec.ts
@@ -9,4 +9,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({});
     expect(error).toEqual(new MissingParamError('any_field'));
   });
+
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut();
+    const error = sut.validate({ any_field: 'any_value' });
+    expect(error).toBeFalsy();
+  });
 });
