95aa17108f9d4b1ae1978b404dd1768a6ceed44d
Dyarlen Iber <dyarlen1@gmail.com>
2021-12-10 17:19:42 +0100

    refactor: rename signup routes with login routes

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from '../../../src/main/config/app';
 import { MongoHelper } from '../../../src/infra/db/mongodb/helpers/mongo-helper';
 
-describe('SignUp routes', () => {
+describe('Login routes', () => {
   beforeAll(async (): Promise<void> => {
     await MongoHelper.connect(<string>process.env.MONGO_URL);
   });
@@ -16,15 +16,17 @@ describe('SignUp routes', () => {
     await accountCollection.deleteMany({});
   });
 
-  test('should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      })
-      .expect(200);
+  describe('POST /signup', () => {
+    test('should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'any_password',
+          passwordConfirmation: 'any_password',
+        })
+        .expect(200);
+    });
   });
 });
