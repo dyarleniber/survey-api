715f0718d74cd98b06fdfda1dc82da9f3db8d1ec
Dyarlen Iber <dyarlen1@gmail.com>
2021-12-12 22:05:58 +0100

    test: add AddSurvey use case to AddSurveyController

--- tests/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ tests/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,7 +1,9 @@
 import { AddSurveyController } from '../../../../../src/presentation/controllers/survey/add-survey/add-survey-controller';
-import { HttpRequest, Validation } from '../../../../../src/presentation/protocols';
+import {
+  HttpRequest, Validation, AddSurvey, AddSurveyModel,
+} from '../../../../../src/presentation/controllers/survey/add-survey/add-survey-controller-protocols';
 import { MissingParamError, ServerError } from '../../../../../src/presentation/errors';
-import { badRequest, serverError } from '../../../../../src/presentation/helpers/http/http-helpers';
+import { badRequest, serverError, noContent } from '../../../../../src/presentation/helpers/http/http-helpers';
 
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
@@ -12,21 +14,41 @@ const makeValidation = (): Validation => {
   return new ValidationStub();
 };
 
+const makeAddSurvey = (): AddSurvey => {
+  class AddSurveyStub implements AddSurvey {
+    async add(_data: AddSurveyModel): Promise<void> {
+      return Promise.resolve();
+    }
+  }
+  return new AddSurveyStub();
+};
+
 const makeFakeRequest = (): HttpRequest => ({
-  body: {},
+  body: {
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer',
+      },
+    ],
+  },
 });
 
 interface SutTypes {
   sut: AddSurveyController;
   validationStub: Validation;
+  addSurveyStub: AddSurvey;
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = makeValidation();
-  const sut = new AddSurveyController(validationStub);
+  const addSurveyStub = makeAddSurvey();
+  const sut = new AddSurveyController(validationStub, addSurveyStub);
   return {
     sut,
     validationStub,
+    addSurveyStub,
   };
 };
 
@@ -39,7 +61,7 @@ describe('AddSurvey Controller', () => {
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
   });
 
-  test('Should return 400 if Validation returns an error', async () => {
+  test('Should return 400 if Validation fails', async () => {
     const { sut, validationStub } = makeSut();
     jest.spyOn(validationStub, 'validate').mockImplementation(() => new MissingParamError('any_field'));
     const httpResponse = await sut.handle(makeFakeRequest());
@@ -53,5 +75,26 @@ describe('AddSurvey Controller', () => {
     });
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new ServerError()));
+  });
+
+  test('Should call AddSurvey with correct values', async () => {
+    const { sut, addSurveyStub } = makeSut();
+    const addSpy = jest.spyOn(addSurveyStub, 'add');
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  test('Should return 500 if AddSurvey throws an error', async () => {
+    const { sut, addSurveyStub } = makeSut();
+    jest.spyOn(addSurveyStub, 'add').mockImplementation(async () => { throw new Error(); });
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new ServerError()));
+  });
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(noContent());
   });
 });
