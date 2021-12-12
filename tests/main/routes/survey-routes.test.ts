7a130480edaa5622cd85548a445a41eda706205a
Dyarlen Iber <dyarlen1@gmail.com>
2021-12-13 00:36:07 +0100

    test: add AddSurvey composition

---
+++ tests/main/routes/survey-routes.test.ts
@@ -0,0 +1,38 @@
+import { Collection } from 'mongodb';
+import request from 'supertest';
+import app from '../../../src/main/config/app';
+import { MongoHelper } from '../../../src/infra/db/mongodb/helpers/mongo-helper';
+
+let surveyCollection: Collection;
+
+describe('Survey routes', () => {
+  beforeAll(async (): Promise<void> => {
+    await MongoHelper.connect(<string>process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany({});
+  });
+
+  describe('POST /surveys', () => {
+    test('should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'image_url',
+          }, {
+            answer: 'Answer 2',
+          }],
+        })
+        .expect(204);
+    });
+  });
+});
