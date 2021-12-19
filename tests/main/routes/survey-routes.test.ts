fb4205168f740063034fc6e4598c7dd20b9d4cab
Dyarlen Iber <dyarlen1@gmail.com>
2021-12-19 11:37:57 +0100

    test: ensure add survey route returns 204 if valid  access token is provided

--- tests/main/routes/survey-routes.test.ts
+++ tests/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
 import { Collection } from 'mongodb';
 import request from 'supertest';
+import { sign } from 'jsonwebtoken';
 import app from '../../../src/main/config/app';
 import { MongoHelper } from '../../../src/infra/db/mongodb/helpers/mongo-helper';
+import env from '../../../src/main/config/env';
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 
 describe('Survey routes', () => {
   beforeAll(async (): Promise<void> => {
@@ -16,7 +19,9 @@ describe('Survey routes', () => {
 
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys');
+    accountCollection = await MongoHelper.getCollection('accounts');
     await surveyCollection.deleteMany({});
+    await accountCollection.deleteMany({});
   });
 
   describe('POST /surveys', () => {
@@ -33,6 +38,39 @@ describe('Survey routes', () => {
           }],
         })
         .expect(403);
+    });
+
+    test('should return 204 on add survey with valid accessToken', async () => {
+      const { insertedId } = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'hashedPassword',
+        role: 'admin',
+      });
+      const accessToken = sign({ id: insertedId.toHexString() }, env.jwtSecret);
+      await accountCollection.updateOne(
+        {
+          _id: insertedId,
+        },
+        {
+          $set: {
+            accessToken,
+          },
+        },
+      );
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'image_url',
+          }, {
+            answer: 'Answer 2',
+          }],
+        })
+        .expect(204);
     });
   });
 });
