6408ff40274ea78c3c1072fcd0333cfff27e79fb
Dyarlen Iber <dyarlen1@gmail.com>
2021-12-23 14:50:17 +0100

    test: add DbLoadSurveys

---
+++ tests/data/use-cases/load-surveys/db-load-surveys.spec.ts
@@ -0,0 +1,81 @@
+import MockDate from 'mockdate';
+import { DbLoadSurveys } from '../../../../src/data/use-cases/load-surveys/db-load-surveys';
+import {
+  SurveyModel,
+  LoadSurveysRepository,
+} from '../../../../src/data/use-cases/load-surveys/db-load-surveys-protocols';
+
+const makeFakeSurveys = (): SurveyModel[] => [
+  {
+    id: 'any_id',
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer',
+      },
+    ],
+    date: new Date(),
+    didAnswer: false,
+  },
+];
+
+const makeLoadSurveysRepository = (): LoadSurveysRepository => {
+  class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+    async loadAll(): Promise<SurveyModel[]> {
+      return makeFakeSurveys();
+    }
+  }
+  return new LoadSurveysRepositoryStub();
+};
+
+interface SutTypes {
+  sut: DbLoadSurveys;
+  loadSurveysRepositoryStub: LoadSurveysRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysRepositoryStub = makeLoadSurveysRepository();
+  const sut = new DbLoadSurveys(loadSurveysRepositoryStub);
+  return {
+    sut,
+    loadSurveysRepositoryStub,
+  };
+};
+
+describe('DbLoadSurveys Use case', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  test('Should call LoadSurveysRepository', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut();
+    const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll');
+    await sut.load();
+    expect(loadAllSpy).toHaveBeenCalled();
+  });
+
+  test('Should throw an error if LoadSurveysRepository throws an error', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut();
+    jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockImplementation(async () => { throw new Error(); });
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow();
+  });
+
+  test('Should return empty if LoadSurveysRepository returns empty', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut();
+    jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockReturnValueOnce(Promise.resolve([]));
+    const surveys = await sut.load();
+    expect(surveys).toEqual([]);
+  });
+
+  test('Should return a list of Surveys on success', async () => {
+    const { sut } = makeSut();
+    const surveys = await sut.load();
+    expect(surveys).toEqual(makeFakeSurveys());
+  });
+});
