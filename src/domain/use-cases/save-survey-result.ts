10f17f6297d771edb75690a9f1766a9147140fa2
Dyarlen Iber <dyarlen1@gmail.com>
2021-12-24 01:32:09 +0100

    feat: add LoadSurveyById and SaveSurveyResult use cases

---
+++ src/domain/use-cases/save-survey-result.ts
@@ -0,0 +1,7 @@
+import { SurveyResultModel } from '@/domain/models/survey-result';
+
+export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>;
+
+export interface SaveSurveyResult {
+  save(survey: SaveSurveyResultModel): Promise<SurveyResultModel>;
+}
